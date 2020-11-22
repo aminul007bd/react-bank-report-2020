b1c923145e23cededbc401fcb80f139d26e792c7
aminul007bd <gem007bd@gmail.com>
2020-11-22 17:51:01 +0100

    add react-bootstrap with json server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import { Container } from 'react-bootstrap'
+
 
 function App() {
   return (
     <div className="App">
+      <h1>Bank Report</h1>
+      <Container fluid>
+        
+        <div className="shadow mt-3 p-3">
+          test
+        </div>
+      </Container>
     </div>
   );
 }
